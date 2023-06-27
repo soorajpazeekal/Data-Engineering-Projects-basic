bb6fca822c6ba064bfc795aa198de1f00d6918b4
soorajpazeekal <soorajpazeekal@gmail.com>
2023-06-27 21:44:41 +0530

    minor changes with gzip transform

--- ETL-With-Postgresql/gzip-transform.py
+++ ETL-With-Postgresql/gzip-transform.py
@@ -25,9 +25,9 @@ def unzip_and_convert_to_gz(zip_file_path, target_file, output_gz_file):
         print("An error occurred:", str(e))
 
 
-for item in os.listdir(config.get("DEFAULT", "DataFolderName")):
-    if (item.endswith(".zip")):
-        zip_file_path = f"{config.get('DEFAULT', 'DataFolderName')}/{item}"; csv_file_path = os.path.splitext(zip_file_path)[0]
+for file in os.listdir(config.get("DEFAULT", "DataFolderName")):
+    if (file.endswith(".zip")):
+        zip_file_path = f"{config.get('DEFAULT', 'DataFolderName')}/{file}"; csv_file_path = os.path.splitext(zip_file_path)[0]
         target_file = os.path.basename(csv_file_path);  output_gz_file = f"{csv_file_path}.gz"
         unzip_and_convert_to_gz(zip_file_path, target_file, output_gz_file)
         break
