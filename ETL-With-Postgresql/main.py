7380108f9c63e8602f2be49250ffc62a0967907f
soorajpazeekal <soorajpazeekal@gmail.com>
2023-06-30 21:42:36 +0530

    minor changes in main.py

--- ETL-With-Postgresql/main.py
+++ ETL-With-Postgresql/main.py
@@ -8,6 +8,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
 
 
+
+
 #Database auth properties
 config = configparser.ConfigParser(); config.read('config.ini')
 def database_conn_properties():
@@ -18,6 +20,9 @@ def database_conn_properties():
         "password": config.get("DATABASE", "ConnectionPassword")
     }
     return database_url, properties, config
+
+
+
 
 #Main declaration for pyspark manager
 class PysparkManager:
@@ -33,6 +38,7 @@ class PysparkManager:
         print("PySpark session stopped")
 
 
+
 spark = PysparkManager().CreateSparkSession()
 
 """
@@ -40,28 +46,56 @@ Extracts data from a list of csv files present in a specified directory (default
 :param directory: A string representing the path of the directory containing the csv files.
 :return: A dataframe containing the data from the csv file/files.
 """
+
+
 def FileExtactPhase(directory = config.get("DEFAULT", "DataFolderName")):
     df = spark.read \
         .format("csv") \
         .option("compression", "gzip") \
         .option("header", True) \
+        .option("inferSchema", "true") \
         .option("path", "{}/*.gz".format(config.get("DEFAULT", "DataFolderName"))) \
         .load()
     return df
 
-def write_database(df, table, move_file=False):
+
+
+def write_database(data_frame, table_name, database_url, properties, move_file=False):
     try:
-        df.write.jdbc(url=database_url, properties=properties, table=table, mode="overwrite")
+        #df.write.jdbc(url=database_url, properties=properties, table=table, mode="overwrite")
+        data_frame.write.format("jdbc") \
+            .option("url", database_url) \
+            .option("dbtable", "warehouse.{}".format(table_name)) \
+            .mode("overwrite") \
+            .options(**properties) \
+            .save()
         print("Database Write Success")
         return df
     except Exception as e:
         print("An error occurred:", str(e))
 
 
-def read_database(table):
+
+
+def read_database(table_name, database_url, properties):
     try:
-        df = spark.read.jdbc(url=database_url, properties=properties, table=table)
+        df = spark.read.format("jdbc") \
+            .option("url", database_url) \
+            .option("dbtable", "warehouse.{}".format(table_name)) \
+            .options(**properties) \
+            .load()
         print("Database Read Success")
         return df
     except Exception as e:
         print("An error occurred:", str(e))
+
+
+
+if __name__ == "__main__":
+    database_url, properties, config = database_conn_properties()
+    df = FileExtactPhase()
+    # df = read_database(table_name="test", database_url=database_url, properties=properties)
+    df.printSchema()
+    # print(df.count())
+    # write_database(data_frame=df, table_name="test", database_url=database_url, properties=properties)
+    PysparkManager.StopSparkSession(spark)
