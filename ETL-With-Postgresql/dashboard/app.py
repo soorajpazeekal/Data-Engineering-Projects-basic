e8f0d28d02998a0221bff8792144a170ec257d0a
Sooraj Pazeekal <soorajpazeekal@gmail.com>
2023-10-05 00:05:19 +0530

    update with dashboard

--- ETL-With-Postgresql/dashboard/app.py
+++ ETL-With-Postgresql/dashboard/app.py
@@ -1,9 +1,31 @@
+import sql as sql
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
 
-st.map(df)
+st.header("citibike Trip data dashboard")
+
+with st.sidebar:
+    option = st.selectbox(
+    'Choose a rideable_type',
+    sql.get_all_bike_types())
+
+col1, col2, col3 = st.columns(3)
+with st.container():
+    col1.metric(label="Count Till date", value=sql.count_bikes_with_names(option), delta=None)
+    value = sql.count_member_types(option)
+    col2.metric(label="Total Member Count VS Casual(User who have valid membership)", 
+                value=value[0], 
+                delta=value[1])
+    value=sql.fav_start_station(option)
+    col3.metric(label="Most Popular Starting Locations", value=value[0], delta=value[1], delta_color="inverse")
+    st.header('Most Popular Starting Locations (by number of bike types)')
+    st.map(sql.plot_map_starting(option))
+    st.header('Most Popular Ending Locations (by number of bike types)')
+    st.map(sql.plot_map_ending(option))
+    
+
---
+++ ETL-With-Postgresql/dashboard/sql.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import psycopg2, configparser
+import pandas as pd
+
+
+config = configparser.ConfigParser(); config.read('/mnt/c/Projects/Data-Engineering-Projects-basic/ETL-With-Postgresql/config.ini')
+conn = psycopg2.connect(
+    dbname = config.get("DATABASE", "dbname"),
+    user = config.get("DATABASE", "ConnectionUser"),
+    password = config.get("DATABASE", "ConnectionPassword"),
+    host = config.get("DATABASE", "host"),
+    port = config.get("DATABASE", "port")
+)
+
+def get_all_bike_types():
+    cursor=conn.cursor()
+    cursor.execute("SELECT DISTINCT rideable_type from warehouse.test")
+    bike_types = []
+    for item in cursor.fetchall():
+        bike_types.append(item[0])
+    cursor.close()
+    return bike_types
+
+def count_bikes_with_names(rideable_type):
+    cursor=conn.cursor()
+    cursor.execute(f"SELECT COUNT(*) from warehouse.test where rideable_type='{rideable_type}'")
+    count = cursor.fetchall()[0][0]
+    cursor.close()
+    return count
+
+def plot_map_starting(rideable_type):
+    cursor=conn.cursor()
+    cursor.execute(f"SELECT start_lat, start_lng from warehouse.test where rideable_type='{rideable_type}'")
+    df = pd.DataFrame(cursor.fetchall(), columns=['lat', 'lon']).dropna()
+    cursor.close()
+    return df
+
+def count_member_types(rideable_type):
+    cursor=conn.cursor()
+    cursor.execute(f"""SELECT member_casual, COUNT(*) AS count
+                FROM warehouse.test where rideable_type='{rideable_type}'
+                GROUP BY member_casual
+                ORDER BY count DESC""")
+    data = cursor.fetchall()
+    member_count = None
+    casual_count = None
+
+    for row in data:
+        if row[0] == 'member':
+            member_count = row[1]
+        elif row[0] == 'casual':
+            casual_count = row[1]
+    cursor.close()
+    return member_count, casual_count
+
+def plot_map_ending(rideable_type):
+    cursor=conn.cursor()
+    cursor.execute(f"SELECT end_lat, end_lng from warehouse.test where rideable_type='{rideable_type}'")
+    df = pd.DataFrame(cursor.fetchall(), columns=['lat', 'lon']).dropna()
+    cursor.close()
+    return df
+
+def fav_start_station(rideable_type):
+    cursor=conn.cursor()
+    cursor.execute(f"""SELECT start_station_name, COUNT(*) AS count
+                    FROM warehouse.test where rideable_type='{rideable_type}'
+                    GROUP BY start_station_name
+                    ORDER BY count DESC""")
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['station_name', 'count']).head(2).dropna()
+    cursor.close()
+    return str(df.loc[0].station_name), str(df.loc[1].station_name)
