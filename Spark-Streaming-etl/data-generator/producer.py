b8a5d5bc99374a1035e5003c9c86ac5ad10df003
Sooraj Pazeekal <soorajpazeekal@gmail.com>
2023-10-11 13:48:34 +0530

    minor changes

--- Spark-Streaming-etl/data-generator/producer.py
+++ Spark-Streaming-etl/data-generator/producer.py
@@ -13,11 +13,13 @@ with open('sams-club-loc.json', 'r') as json_file:
 
 
 def need_data():
+    location = json_data_from_file[f"location_{random.randint(1, 5)}"]
     data = {
     "Order_id": fake.uuid4(),
     "Name": fake.name(),
-    "Address": fake.address(),
-    "Location": json_data_from_file[f"location_{random.randint(1, 5)}"],
+    "Email": fake.email(),
+    "latitude": float(location["lat"]),
+    "longitude": location["lon"],
     "created_at": time.time()
     }
     json_data = json.dumps(data)
